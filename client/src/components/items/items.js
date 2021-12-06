9ea241e1b031b7fac3536b9e1686964e1abc8fd8
mm1089 <mm1089@canterbury.ac.uk>
2021-12-06 17:02:18 +0000

    [14]
    - removed tailwind import.

--- client/src/components/items/items.js
+++ client/src/components/items/items.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, Component } from "react";
-import "tailwindcss/tailwind.css";
 
 function Items() {
   return (
@@ -17,7 +16,7 @@ function Items() {
           <label >Username: <input placeholder="username" className="border focus:outline-none hover:shadow-md cursor-pointer p-1 rounded bg-gray-100 shadow" type="text" name="username" /></label>
           <label className="p-1">Latitude: <input placeholder="latitude" className="border focus:outline-none hover:shadow-md cursor-pointer p-1 rounded bg-gray-100 shadow" type="text" name="latitude" /></label>
           <label className="p-1">Longitude: <input placeholder="longitude" className="border focus:outline-none hover:shadow-md cursor-pointer p-1 rounded bg-gray-100 shadow" type="text" name="longitude" /></label>
-          <label className="p-1">Image: <input placeholder="image" placeholder="http://placekitten.com/100/100"className="border focus:outline-none hover:shadow-md cursor-pointer p-1 rounded bg-gray-100 shadow" type="text" name="image" /></label>
+          <label className="p-1">Image: <input placeholder="http://placekitten.com/100/100"className="border focus:outline-none hover:shadow-md cursor-pointer p-1 rounded bg-gray-100 shadow" type="text" name="image" /></label>
           <label className="p-1">Keywords: <input placeholder="keywords" className="border focus:outline-none hover:shadow-md cursor-pointer p-1 rounded bg-gray-100 shadow" type="text" name="keywords" /></label>
           <label className="p-1">Description: <input placeholder="description" className="border focus:outline-none hover:shadow-md cursor-pointer p-1 rounded bg-gray-100 shadow" type="text" name="description" /></label>
           <input className="hover:bg-green-100 hover:text-gray-800 transition ease-out duration-300 ml-2 font-bold focus:outline-none active:scale-95 hover:shadow-md cursor-pointer bg-gray-200 rounded-md p-2 "type="submit" value="Submit" />
