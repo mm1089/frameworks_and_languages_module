8147f5ee6187482df97bddea109fa18f78613c83
mm1089 <mm1089@canterbury.ac.uk>
2022-01-03 16:07:57 +0000

    > Created Item.js Started CORS default setup for  item Added second item in hardcoded items

--- server/items.js
+++ server/items.js
@@ -1,13 +1,22 @@
 var items = {
   1: {
       "id": 1,
-      "user_id": "michael",
+      "user_id": "Michael",
       "keywords": ["car", "coffee", "zebra"],
       "description": "amazing description",
       "lat": 1,
       "lon": 1,
       "date_from": "2021-11-22T08:22:39.067408",
-  }
+  },
+  2: {
+    "id": 2,
+    "user_id": "McLain",
+    "keywords": ["3070ti", "3800x", "AX1200i"],
+    "description": "Amazing pc",
+    "lat": 2,
+    "lon": 2,
+    "date_from": "2020-01-22T08:22:39.028283",
+}
 }
 
 
---
+++ server/routes/item.js
@@ -0,0 +1,16 @@
+var express = require('express')
+var cors = require('cors')
+var item = require ('../items')
+
+//require hardcoded items
+//require the express package
+//require the cors package
+//Resources Used: 
+//https://www.npmjs.com/package/cors
+//https://www.youtube.com/watch?v=PNtFSVU-YTI
+//origin * is totally safe unless that resource contains private data.
+//Cross-Origin Resource Sharing (CORS) Default Setup
+
+var corsDefault = {
+    origin: '*',
+}
