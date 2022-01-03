3a09e180cffbd359d0ef254ffcea4d3d96c6ba6f
mm1089 <mm1089@canterbury.ac.uk>
2022-01-03 21:04:54 +0000

    > Created more dummy data

--- server/items.js
+++ server/items.js
@@ -3,12 +3,34 @@ var items = {
       "id": 1,
       "user_id": "Michael",
       "keywords": ["car", "coffee", "zebra"],
-      "description": "amazing description",
+      "description": "Amazing description",
       "lat": 1,
       "lon": 1,
-      "date_from": "2021-11-22T08:22:39.067408",
   },
-
+  2: {
+    "id": 2,
+    "user_id": "McLain",
+    "keywords": ["3070ti", "3800x", "AX1200i"],
+    "description": "Amazing pc",
+    "lat": 2,
+    "lon": 2,
+  },
+  3: {
+    "id": 3,
+    "user_id": "Allan",
+    "keywords": ["Test", "Test", "Test"],
+    "description": "Amazing Allan",
+    "lat": 2,
+    "lon": 2,
+  },
+  4: {
+    "id": 4,
+    "user_id": "test",
+    "keywords": ["cant", "think", "dummy"],
+    "description": "Amazing waffle",
+    "lat": 2,
+    "lon": 2,
+}
 }
 
 
