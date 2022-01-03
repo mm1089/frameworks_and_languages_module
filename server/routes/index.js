475d1e14833a3255772f9808f79cb8e68aa49925
mm1089 <mm1089@canterbury.ac.uk>
2022-01-03 15:42:33 +0000

    > corsOptions, origin, methods & allowed headers. http 200 for successful req. Temporary listener to test

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,25 @@
 //Attempting Routes
 var express = require('express')
-//require the express package
 var cors = require('cors');
+var app = express()
+
+//require the express package
 //require the cors package
-//router
+//Resource Used: https://www.npmjs.com/package/cors
+
+var corsOptions = {
+  origin: '*',
+  methods:'GET, POST, DELETE, OPTIONS',
+  AllowedHeaders: 'Content-Type',
+  optionsSuccessStatus: 204
+}
+
+//The HTTP 200 OK success status response code indicates that the request has succeeded.
+
+app.get('/products/:id', cors(corsOptions), function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for only example.com.'})
+})
+ 
+app.listen(8000, function () {
+  console.log('CORS-enabled web server listening on port 80')
+})
